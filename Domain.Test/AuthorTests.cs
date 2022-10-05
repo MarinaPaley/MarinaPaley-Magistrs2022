ed227c3f85ddba6d810921e151dc36ce143d4152
MarinaPaley <marina_paley@mail.ru>
2022-10-05 18:43:55 +0300

    Начало работы с тестами

---
+++ Domain.Test/AuthorTests.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="AuthorTests.cs" company="Васильева Марина Алексеевна">
+// Copyright (c) Васильева Марина Алексеевна 2022. Учебные материалы.
+// </copyright>
+namespace Domain.Test
+{
+    using Domain;
+
+    [TestFixture]
+    /// <summary>
+    /// Тесты для класса Автор.
+    /// </summary>
+    public class AuthorTests
+    {
+        /// <summary>
+        /// Тест на конструктор с правильным ФИО.
+        /// </summary>
+        [Test]
+        public void Ctor_Valid_DoesnotThrowException()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.DoesNotThrow(() => _ = new Author("Пушкин", "Александр", "Сергеевич"));
+        }
+    }
+}
